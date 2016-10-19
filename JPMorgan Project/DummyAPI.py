50bb459f98f21c6aeb320e1185e7532c9e92edae
XucanWang <xucanwang93@gmail.com>
2016-10-18 23:13:34 -0400

    add flash notification

--- JPMorgan Project/DummyAPI.py
+++ JPMorgan Project/DummyAPI.py
@@ -1,31 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for, redirect
 import httplib, urllib2
 import random
 import json
 
 app = Flask(__name__)
-
+app.secret_key = "super secret key"
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
 ORDER = "http://localhost:8080/order?id={}&side=sell&qty={}&price={}"
 
 
 @app.route("/")
-def showHomePage():
-    return render_template('main.html')
+def show_homepage():
+    flash('flashTest!!!!!')
+    return render_template('homepage.html')
 
 
-@app.route("/login", methods=['GET','POST'])
-def LoginProcess():
-    attempted_username = request.form['username']
-    attempted_password = request.form['password']
-    print attempted_password
-    print attempted_username
-    return render_template('success.html')
+@app.route("/login", methods=['GET', 'POST'])
+def login_process():
+    error = ''
+    try:
+        if request.method == "POST":
+            attempted_username = request.form['username']
+            attempted_password = request.form['password']
+
+        if attempted_username == "wangxucan" and attempted_password == "password":
+            flash("You have successfully logged in")
+            return redirect(url_for('show_homepage'))
+        else:
+            flash("Invalid Credentials, Try Again!")
+            return render_template('homepage.html')
+
+
+    except Exception as e:
+        flash(e)
+        return render_template('homepage.html')
 
 
 @app.route("/sell_action", methods=['POST'])
-def sellStock():
+def sell_stock():
     price = int(request.form['price'])
     quantity = int(request.form['quantity'])
     order_parameters = (price, quantity)
@@ -42,4 +55,5 @@ def sellStock():
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
